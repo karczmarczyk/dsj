b2276fa082614b4df987a33be3ea86a2c00a2fa5
Optiplex <mateusz.karczmarczyk@gmail.com>
2017-02-06 23:01:09 +0100

    ini

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=dsj',
+    'username' => 'postgres',
+    'password' => 'power1',
+    'charset' => 'utf8',
+];
